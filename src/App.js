6b7413701609cb1b79bc6f602449d44b8ca92b04
Shrusti <shrustiparmar1411@gmail.com>
2022-02-15 12:40:18 +0530

    Dummy react project using webpack from scratch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+// import {hot} from "react-hot-loader";
 import "./App.css";
 
 class App extends Component{
@@ -6,6 +7,8 @@ class App extends Component{
     return(
       <div className="App">
         <h1> Hello, World! </h1>
+        <h1>SHRUSTI PARMAR</h1>
+        <h1>here!!</h1>
       </div>
     );
   }
